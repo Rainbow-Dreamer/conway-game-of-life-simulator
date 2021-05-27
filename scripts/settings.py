fa899b7e5c8b90c633e77e534ddce87211b8d1c6
Rainbow Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2021-05-27 16:19:24 +0800

    update

---
+++ scripts/settings.py
@@ -0,0 +1,8 @@
+B = 3
+S = [2, 3]
+title_name = 'conway\'s game of life simulator'
+screen_size = 1000, 600
+block_size = 10
+background_img = 'resources/background_black.png'
+life_img = 'resources/block_white.png'
+read_file = None
